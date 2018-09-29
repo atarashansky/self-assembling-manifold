cb54b5ed14c01f81bace517c2e7d95f5cf0be055
atarashansky <tarashan@stanford.edu>
2018-09-28 17:43:53 -0700

    Update .travis_before_install.sh

--- .travis_before_install.sh
+++ .travis_before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $TRAVIS_OS_NAME == 'linux' ]; then
   echo "Installing deps for linux"
-  sudo apt-get install libhdf5-10
+  sudo apt-get update
   sudo apt-get install libhdf5-dev
   #sudo add-apt-repository ppa:nschloe/swig-backports -y
   #sudo apt-get -qq update
