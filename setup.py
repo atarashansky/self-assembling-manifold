f3b753dfc32fdc0576511b01342a19920e9164a1
Alec Joel Tarashansky <tarashan@stanford.edu>
2018-09-29 19:32:27 -0700

    added numba requirement

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
 
     py_modules=["SAM","utilities"],
     
-    install_requires=['pandas','numpy>=1.15','scikit-learn>=0.20','matplotlib','scipy','anndata','scanpy','louvain']
+    install_requires=['pandas','numpy>=1.15','scikit-learn>=0.20','matplotlib','scipy','anndata','scanpy','louvain','numba>=0.37,<0.40']
 )
