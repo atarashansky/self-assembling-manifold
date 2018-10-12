8f63ab258b59b2934465f2b49c3690a9629e3223
atarashansky <tarashan@stanford.edu>
2018-10-11 19:37:01 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
 
     py_modules=["SAM","utilities"],
     
-    install_requires=['pandas','numpy>=1.15','scikit-learn>=0.20','matplotlib','scipy','anndata','numba>=0.37,<0.40','umap-learn'],
+    install_requires=['pandas','numpy>=1.15','scikit-learn>=0.20','matplotlib','scipy','numba>=0.37,<0.40','umap-learn'],
 
-    extras_require={'louvain': ['scanpy', 'louvain']},
+    extras_require={'louvain': ['scanpy', 'louvain','anndata']},
 )
