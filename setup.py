5178f04fb9f9070dbf4be86d84315aaed3471bd4
Fabio Zanini <fabio.zanini@fastmail.fm>
2018-10-12 12:50:15 -0700

    Tidy up setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
 from setuptools import setup
 
 setup(
-
-    name='sam-algorithm', 
-
-    version='0.1.9',  
-
-    description='The Self-Assembling-Manifold algorithm', 
-
-    long_description="The Self-Assembling-Manifold algorithm for analyzing single-cell RNA sequencing data.",  
-
-    long_description_content_type='text/markdown',  
-    
-    url='https://github.com/atarashansky/self-assembling-manifold',  
-
-    author='Alexander J. Tarashansky',  
-
-    author_email='tarashan@stanford.edu',  
-
-    keywords='scrnaseq analysis manifold reconstruction',  
-
-    py_modules=["SAM","utilities"],
-    
-    install_requires=['pandas','numpy>=1.15','scikit-learn>=0.20','matplotlib','scipy','anndata','numba>=0.37,<0.40','umap-learn'],
-
-    extras_require={'louvain': ['scanpy', 'louvain']},
+    name='sam-algorithm',
+    version='0.1.9',
+    description='The Self-Assembling-Manifold algorithm',
+    long_description="The Self-Assembling-Manifold algorithm for analyzing single-cell RNA sequencing data.",
+    long_description_content_type='text/markdown',
+    url='https://github.com/atarashansky/self-assembling-manifold',
+    author='Alexander J. Tarashansky',
+    author_email='tarashan@stanford.edu',
+    keywords='scrnaseq analysis manifold reconstruction',
+    py_modules=["SAM", "utilities"],
+    install_requires=[
+        'pandas',
+        'numpy>=1.15',
+        'scikit-learn>=0.20',
+        'matplotlib',
+        'scipy',
+        'anndata',
+        'numba>=0.37,<0.40',
+        'umap-learn'],
+    extras_require={
+        'louvain': ['scanpy', 'louvain'],
+        },
 )
