77b4adc48812dc98bb631ca565303958ffde350a
atarashansky <tarashan@stanford.edu>
2021-02-26 11:46:57 -0800

    update version

--- samalg/__init__.py
+++ samalg/__init__.py
@@ -15,7 +15,7 @@ import warnings
 from numba.core.errors import NumbaWarning
 warnings.filterwarnings("ignore", category=NumbaWarning)
 
-__version__ = "0.8.4"
+__version__ = "0.8.5"
 
 """
 Copyright 2018, Alexander J. Tarashansky, All rights reserved.
