73c6f425f6eb04452c4ef5aca2e42253111092c8
atarashansky <atarashansky@CZIMACOS3990.hsd1.ma.comcast.net>
2022-11-03 20:43:41 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sam-algorithm",
-    version="1.0.1",
+    version="1.0.2",
     description="The Self-Assembling-Manifold algorithm",
     long_description="The Self-Assembling-Manifold algorithm for analyzing single-cell RNA sequencing data.",
     long_description_content_type="text/markdown",
